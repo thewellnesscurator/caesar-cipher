1cbf7bca5a531df32c0d96e5a3e5c5e8899dbd55
thewellnesscurator <anniembailey@gmail.com>
2024-11-17 19:34:21 -0500

    Annie's version of Ceaser Cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,25 @@
-# add your code here
+text = input("Please enter a sentence:")
+text = text.lower()
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+shift = 5 
+
+cipherbet = {}
+
+for i in range(len(alphabet)):
+    old_letter = alphabet[i]
+    position_shift = (i+shift)%26
+    new_letter = alphabet[position_shift]
+    cipherbet[old_letter]= new_letter
+
+print("Cipher dictionary:", cipherbet)
+
+coded_text = ""
+
+for char in text:
+    if char in cipherbet:
+        coded_text += cipherbet[char]
+    else:
+        coded_text += char
+
+print("Coded text:",coded_text)       
